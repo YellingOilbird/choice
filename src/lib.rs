54d041e82e864db243b6e639fe6a69a5baaa343f
YellingOilbird <lysenkori@gmail.com>
2022-05-01 18:32:33 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,7 @@ impl Contract {
         let deposit = env::attached_deposit();
         assert!(
             deposit >= CREATOR_BOND,
-        	"You need have at least 10Ⓝ to create proposal" 
+            "You need have at least 10Ⓝ to create proposal" 
         );
 
         assert!(funds >= CREATOR_BOND, "Min deposit for proposal = {}Ⓝ", CREATOR_BOND);
