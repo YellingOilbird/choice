70dfdbcd952db593fa3e6728a4f4442da01d55a5
YellingOilbird <lysenkori@gmail.com>
2022-05-01 18:34:01 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -127,8 +127,8 @@ impl Contract {
 
         let predecessor = env::predecessor_account_id();
         assert!(
-        	self.choicers.contains_key(&predecessor),
-        	"You must create a membership first to push your proposals"
+			self.choicers.contains_key(&predecessor),
+			"You must create a membership first to push your proposals"
         );
 
         let deposit = env::attached_deposit();
